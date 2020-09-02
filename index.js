51e6a7b8a03d570eb44453415744fc7e70f8c466
Wen Jie <wenjiens15@gmail.com>
2020-09-02 21:15:25 +0800

    update id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = '/peerServer'
 const port = process.env.PORT || 4000
 const host = process.env.HOST || '0.peerjs.com'
 
-const peerServer = PeerServer({ host, port, path, debug: 3})
+const peerServer = new PeerServer(undefined, { host, port, path, debug: 3})
 
 
 console.log(`port: ${port}\npath: ${path}\nhost: ${host}`)
