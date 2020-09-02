093514977a450cc8a346623928f2db24437d8b8b
Wen Jie <wenjiens15@gmail.com>
2020-09-02 21:05:07 +0800

    debuig

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { PeerServer } = require('peer');
 const path = '/peerServer'
 const port = process.env.PORT || 4000
 
-const peerServer = PeerServer({ port, path})
+const peerServer = PeerServer({ port, path, debug: 3})
 
 
 console.log(`port: ${port}\npath: ${path}`)
